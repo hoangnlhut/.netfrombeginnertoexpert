65136f06509a55d48fc6c10dbc9703101780592d
hoangnguyenle.hnlhut <hoangnguyenle.hnlhut@gmail>
2023-06-06 18:34:02 +0700

    commit first project "Working with C# Records" into this repository

---
+++ WorkingWithC#9Records/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkingWithC_9Records
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
