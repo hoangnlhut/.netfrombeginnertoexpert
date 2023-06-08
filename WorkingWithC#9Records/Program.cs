f6457d8641eb47a9f711f4dd4f66dcf834df0a44
DESKTOP-J76PCRA\Hoang <hoangnguyenle.hnlhut@gmail.com>
2023-06-08 10:54:40 +0700

    push working with c# record

--- WorkingWithC#9Records/Program.cs
+++ WorkingWithC#9Records/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         { 
-            Console.WriteLine("Hello, Worldaaaaaa!"); 
+            Console.WriteLine("Hello, Worldaaaa!"); 
         }
     }
 }
